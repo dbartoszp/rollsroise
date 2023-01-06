9895d3eb304a251d4864c71211e193c32351aea7
dbartoszp <dbartosz.pomierny@wp.pl>
2023-01-06 19:23:16 +0100

    js initial commmit

---
+++ resources/js/main.js
@@ -0,0 +1,4 @@
+const navigation = document.querySelector(".main-nav");
+const btnRoise = document.querySelector(".btn btn-roise");
+const btnBlue = document.querySelector(".btn btn-blue");
+const imgAnatomy = document.querySelector(".roise-anatomy");
